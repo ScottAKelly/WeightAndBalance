0147ba894ca34a09fe2ced05ba0135ab10e9a6dc
Scott Kelly <sctkelly@gmail.com>
2016-06-08 15:53:26 -0400

    Added a few entities.

---
+++ WeightAndBalance/WeightAndBalance.Data/PayloadItems.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WeightAndBalance.Data
+{
+    public class PayloadItems
+    {
+        [Key]
+        public int PayloadItemId { get; set; }
+        public float PayloadItemWeight { get; set; }
+        public float PayloadItemMoment { get; set; }
+        public float PayloadItemArm { get; set; }
+    }
+}
