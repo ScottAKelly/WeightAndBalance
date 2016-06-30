cb508639fe4c5d25aa4bf8dea0bc8045da8fe88d
Scott Kelly <sctkelly@gmail.com>
2016-06-30 16:42:36 -0400

    Added migration to hopefully fix Db being disposed problem but it didn't help.

---
+++ WeightAndBalanceApp/WeightAndBalanceApp.Data/Migrations/201606302040132_updateForPayload.cs
@@ -0,0 +1,16 @@
+namespace WeightAndBalanceApp.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateForPayload : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
